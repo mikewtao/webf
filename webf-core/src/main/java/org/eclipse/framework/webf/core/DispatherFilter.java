1cb5ee48b440ca2d22ac304c9472dd68960fb388
mikewt <forwangtao@163.com>
2017-10-23 22:07:46 +0800

    优化代码格式

--- webf-core/src/main/java/org/eclipse/framework/webf/core/DispatherFilter.java
+++ webf-core/src/main/java/org/eclipse/framework/webf/core/DispatherFilter.java
@@ -1,8 +1,8 @@
 package org.eclipse.framework.webf.core;
 
-import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -27,9 +26,6 @@ import org.eclipse.framework.webf.core.utils.ClassScan;
 import org.eclipse.framework.webf.core.utils.JavaassitUtil;
 
 import com.alibaba.fastjson.JSONArray;
-
-
-
 
 public class DispatherFilter implements Filter {
 	private static final Map<String, RequestParam> map = new HashMap<String, RequestParam>();
@@ -57,91 +53,99 @@ public class DispatherFilter implements Filter {
 				Class<?>[] paramClzz = m.getParameterTypes();
 				String[] names = JavaassitUtil.getParams(obj.getClass(), m.getName());
 				List<Object> methodParams = new ArrayList<Object>();
-				for (int i = 0; i < paramClzz.length; i++) {// HttpServletRequest
-															// HttpServletResponse
-					String pName = paramClzz[i].getSimpleName();
-					String reqVal = res.getParameter(names[i]);
-					// 注入request
-					if (pName.equals("HttpServletRequest")) {
-						methodParams.add(res);
-						continue;
-						// 注入response
-					} else if (pName.equals("HttpServletResponse")) {
-						methodParams.add(resp);
-						continue;
-						// 注入string
-					} else if (pName.equals("String")) {
-						methodParams.add(reqVal);
-						continue;
-						// 注入基本类型 int double float boolean byte char
-					} else if (pName.equals("int")) {
-						methodParams.add(Integer.valueOf(reqVal));
-						continue;
-					} else if (pName.equals("double")) {
-						methodParams.add(Double.valueOf(reqVal));
-						continue;
-					} else if (pName.equals("float")) {
-						methodParams.add(Float.valueOf(reqVal));
-						continue;
-					} else {
-						Field[] fields = paramClzz[i].getDeclaredFields();
-						Object ojb = paramClzz[i].newInstance();
-						for (Field f : fields) {
-							f.setAccessible(true);
-							String pjName = f.getType().getSimpleName();
-							String pval = res.getParameter(f.getName());
-							String fieldName = f.getName().substring(0, 1).toUpperCase() + f.getName().substring(1);
-							Method pm = paramClzz[i].getMethod("set" + fieldName, f.getType().newInstance().getClass());
-							if (pjName.equals("String")) {
-								pm.invoke(ojb, pval);
-							} else if (pjName.equals("int")) {
-								pm.invoke(ojb, Integer.valueOf(pval));
-							} else if (pjName.equals("double")) {
-								pm.invoke(ojb, Double.valueOf(pval));
-								continue;
-							} else if (pjName.equals("float")) {
-								pm.invoke(ojb, Float.valueOf(pval));
-								continue;
-							}
-						}
-						methodParams.add(ojb);
-						continue;
-					}
+				for (int i = 0; i < paramClzz.length; i++) {
+					injectParam(res, resp, paramClzz, names, methodParams, i);
 
 				}
-				// 没有返回值
-				if (m.getReturnType() == void.class) {
-					m.invoke(obj, methodParams.toArray());
-					// 返回string 试图
-				} else if (m.getReturnType() == String.class) {
-					String path = (String) m.invoke(obj, methodParams.toArray());
-					path = "../" + path;
-					request.getRequestDispatcher(path).forward(res, resp);
-				} else {
-					// pojo 转换成json
-					Object jobj = m.invoke(obj, methodParams.toArray());
-					JSON json = m.getAnnotation(JSON.class);
-					if (json != null) {
-						String dateType = json.DateType();
-						resp.setContentType("text/html;charset=UTF-8");
-						resp.setCharacterEncoding("utf-8");
-						resp.setHeader("Pragma", "no-cache");
-						resp.setHeader("Cache-Control", "no-cache, must-revalidate");
-						resp.setHeader("Pragma", "no-cache");
-						try {
-							response.getWriter().write(JSONArray.toJSONStringWithDateFormat(jobj, dateType));
-							response.getWriter().flush();
-							response.getWriter().close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
+				fowardView(request, response, res, resp, obj, m, methodParams);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}else{
 			res.getRequestDispatcher("error.html").forward(res, resp);
+		}
+	}
+
+	private void injectParam(HttpServletRequest res, HttpServletResponse resp, Class<?>[] paramClzz, String[] names,
+			List<Object> methodParams, int i)
+			throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+		String pName = paramClzz[i].getSimpleName();
+		String reqVal = res.getParameter(names[i]);
+		// 注入request
+		if (pName.equals("HttpServletRequest")) {
+			methodParams.add(res);
+			return;
+		} else if (pName.equals("HttpServletResponse")) {
+			methodParams.add(resp);
+			return;
+		} else if (pName.equals("String")) {
+			methodParams.add(reqVal);
+			return;
+		} else if (pName.equals("int")) {
+			methodParams.add(Integer.valueOf(reqVal));
+			return;
+		} else if (pName.equals("double")) {
+			methodParams.add(Double.valueOf(reqVal));
+			return;
+		} else if (pName.equals("float")) {
+			methodParams.add(Float.valueOf(reqVal));
+			return;
+		} else {
+			Field[] fields = paramClzz[i].getDeclaredFields();
+			Object ojb = paramClzz[i].newInstance();
+			for (Field f : fields) {
+				f.setAccessible(true);
+				String pjName = f.getType().getSimpleName();
+				String pval = res.getParameter(f.getName());
+				String fieldName = f.getName().substring(0, 1).toUpperCase() + f.getName().substring(1);
+				Method pm = paramClzz[i].getMethod("set" + fieldName, f.getType().newInstance().getClass());
+				if (pjName.equals("String")) {
+					pm.invoke(ojb, pval);
+				} else if (pjName.equals("int")) {
+					pm.invoke(ojb, Integer.valueOf(pval));
+				} else if (pjName.equals("double")) {
+					pm.invoke(ojb, Double.valueOf(pval));
+					continue;
+				} else if (pjName.equals("float")) {
+					pm.invoke(ojb, Float.valueOf(pval));
+					continue;
+				}
+			}
+			methodParams.add(ojb);
+			return;
+		}
+	}
+
+	private void fowardView(ServletRequest request, ServletResponse response, HttpServletRequest res,
+			HttpServletResponse resp, Object obj, Method m, List<Object> methodParams)
+			throws IllegalAccessException, InvocationTargetException, ServletException, IOException {
+		// 没有返回值
+		if (m.getReturnType() == void.class) {
+			m.invoke(obj, methodParams.toArray());
+			// 返回string 试图
+		} else if (m.getReturnType() == String.class) {
+			String path = (String) m.invoke(obj, methodParams.toArray());
+			path = "../" + path;
+			request.getRequestDispatcher(path).forward(res, resp);
+		} else {
+			// pojo 转换成json
+			Object jobj = m.invoke(obj, methodParams.toArray());
+			JSON json = m.getAnnotation(JSON.class);
+			if (json != null) {
+				String dateType = json.DateType();
+				resp.setContentType("text/html;charset=UTF-8");
+				resp.setCharacterEncoding("utf-8");
+				resp.setHeader("Pragma", "no-cache");
+				resp.setHeader("Cache-Control", "no-cache, must-revalidate");
+				resp.setHeader("Pragma", "no-cache");
+				try {
+					response.getWriter().write(JSONArray.toJSONStringWithDateFormat(jobj, dateType));
+					response.getWriter().flush();
+					response.getWriter().close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
@@ -181,36 +185,41 @@ public class DispatherFilter implements Filter {
 						}
 					}
 					for(Field field:fields){
-						Inject inject=field.getAnnotation(Inject.class);
-						if(inject!=null){
-							field.setAccessible(true);
-							List<Class<?>> namedClzzlist=classScan.getClassByInterface(scanPackage, field.getType());
-			            	//此处根据接口查找实现类  ClassScan
-							System.out.println(field.getType().getName());
-			            	Class<?> fieldCls=namedClzzlist.get(0);
-			            	//初始化service
-			            	Object serviceObj=fieldCls.newInstance();
-			            	Field[] serviceFields=serviceObj.getClass().getDeclaredFields();
-			            	for(Field serviceField:serviceFields){
-			            		Inject serviceInject=serviceField.getAnnotation(Inject.class);
-			            		if(serviceInject!=null){
-			            			List<Class<?>> serviceClzzlist=classScan.getClassByInterface(scanPackage, serviceField.getType());
-			            			Class<?> servicefieldCls=serviceClzzlist.get(0);
-			            			//初始dao
-			            			Object daoObj=servicefieldCls.newInstance();
-			            			serviceField.setAccessible(true);
-			            			serviceField.set(serviceObj, daoObj);
-			            		}
-			            	}
-			            	//给字段设值
-			            	field.set(obj,serviceObj);
-						}
+						injectClass(scanPackage, classScan, obj, field);
 					}
 				}
 			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new RuntimeException("initilized error");
+		}
+	}
+
+	private void injectClass(String scanPackage, ClassScan classScan, Object obj, Field field)
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		Inject inject=field.getAnnotation(Inject.class);
+		if(inject!=null){
+			field.setAccessible(true);
+			List<Class<?>> namedClzzlist=classScan.getClassByInterface(scanPackage, field.getType());
+			//此处根据接口查找实现类  ClassScan
+			System.out.println(field.getType().getName());
+			Class<?> fieldCls=namedClzzlist.get(0);
+			//初始化service
+			Object serviceObj=fieldCls.newInstance();
+			Field[] serviceFields=serviceObj.getClass().getDeclaredFields();
+			for(Field serviceField:serviceFields){
+				Inject serviceInject=serviceField.getAnnotation(Inject.class);
+				if(serviceInject!=null){
+					List<Class<?>> serviceClzzlist=classScan.getClassByInterface(scanPackage, serviceField.getType());
+					Class<?> servicefieldCls=serviceClzzlist.get(0);
+					//初始dao
+					Object daoObj=servicefieldCls.newInstance();
+					serviceField.setAccessible(true);
+					serviceField.set(serviceObj, daoObj);
+				}
+			}
+			//给字段设值
+			field.set(obj,serviceObj);
 		}
 	}
 
